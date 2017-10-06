94e92acc14845dd27884407940c621f4ee4ccfcf
Dalton Claybrook <daltonclaybrook@gmail.com>
2017-10-06 14:04:21 -0500

    Initial commit

---
+++ RxSwiftMVVM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxSwiftMVVM
+//
+//  Created by Dalton Claybrook on 10/6/17.
+//  Copyright © 2017 Dalton Claybrook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
